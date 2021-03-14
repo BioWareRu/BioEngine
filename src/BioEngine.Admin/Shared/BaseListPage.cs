3a550e715b829370772eabf5bf837f6fb7b1e30e
George Drak <sonicgd@gmail.com>
2021-03-14 17:43:25 +0500

    Add titles, fix sorting

--- src/BioEngine.Admin/Shared/BaseListPage.cs
+++ src/BioEngine.Admin/Shared/BaseListPage.cs
@@ -11,7 +11,6 @@ namespace BioEngine.Admin.Shared
 {
     public abstract class BaseListPage<TItem, TItemPk> : BasePage where TItem : class, IEntity<TItemPk>
     {
-        private IRepository<TItem, TItemPk> Repository { get; set; }
         [Inject] protected NavigationManager NavigationManager { get; set; }
 
         protected IEnumerable<TItem> Items;
@@ -20,24 +19,23 @@ namespace BioEngine.Admin.Shared
         protected int _pageIndex = 1;
         protected int _pageSize = 20;
 
-        protected override Task OnInitializedAsync()
-        {
-            Repository = ScopedServices.GetRequiredService<IRepository<TItem, TItemPk>>();
-            return base.OnInitializedAsync();
-        }
-
         protected async Task OnChange(QueryModel<TItem> queryModel)
         {
             var orderBy = string.Join(",",
-                queryModel.SortModel.Where(s => s.Sort != null && s.Sort != SortDirection.None.Name)
+                queryModel.SortModel
+                    .Where(s => s.Sort is not null)
                     .OrderBy(s => s.Priority)
-                    .Select(s => $"{(s.Sort == SortDirection.Descending.Name ? "-" : "")}{s.FieldName}"));
-            await LoadData(orderBy, queryModel.PageSize * (queryModel.PageIndex - 1), queryModel.PageSize);
+                    .Select(s =>
+                        $"{(s.Sort == SortDirection.Descending.Name ? "-" : "")}{s.FieldName.ToLowerInvariant()}"));
+            await LoadData(string.IsNullOrEmpty(orderBy) ? "-title" : orderBy,
+                queryModel.PageSize * (queryModel.PageIndex - 1), queryModel.PageSize);
         }
 
-        protected async Task LoadData(string orderBy = "-title", int skip = 0, int take = 20)
+        private async Task LoadData(string orderBy, int skip = 0, int take = 20)
         {
-            var results = await Repository.GetAllAsync(query =>
+            using var scope = ScopedServices.CreateScope();
+            var repository = scope.ServiceProvider.GetRequiredService<IRepository<TItem, TItemPk>>();
+            var results = await repository.GetAllAsync(query =>
                 query.Take(take)
                     .Skip(skip)
                     .OrderByString(orderBy)
