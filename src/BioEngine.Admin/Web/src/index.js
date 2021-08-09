10f0090482ca260e5424aef38f087beb9e6406f3
George Drak <sonicgd@gmail.com>
2021-08-09 17:50:40 +0500

    Font Awesome via svg and js to load only used icons

--- src/BioEngine.Admin/Web/src/index.js
+++ src/BioEngine.Admin/Web/src/index.js
@@ -1,1 +1,16 @@
 import './index.scss';
+import {library, dom, config} from '@fortawesome/fontawesome-svg-core'
+import {
+  faTachometerAlt,
+  faFolder,
+  faCopyright,
+  faEdit,
+  faList,
+  faPlus,
+  faTh,
+  faUsers, faGamepad, faComments, faFile, faGlobe, faTags, faDatabase, faTrash, faCopy, faUpload, faFolderPlus, faSync
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faTachometerAlt, faFolder, faCopyright, faEdit, faList, faPlus, faTh, faUsers, faGamepad, faComments, faFile, faGlobe, faTags, faDatabase, faTrash, faCopy, faUpload, faFolderPlus, faSync)
+config.keepOriginalSource = false;
+dom.watch()
