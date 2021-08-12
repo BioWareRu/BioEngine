ca56d4293dc02faf0dbc86407c309c62029ea07a
George Drak <sonicgd@gmail.com>
2021-08-13 00:29:51 +0500

    Add redis data protection support

--- src/BioEngine.Core/BioEngineStartup.cs
+++ src/BioEngine.Core/BioEngineStartup.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Sitko.Core.Blazor.AntDesignComponents;
+using StackExchange.Redis;
 
 namespace BioEngine.Core
 {
@@ -10,5 +12,31 @@ namespace BioEngine.Core
             configuration, environment)
         {
         }
+
+        protected override IDataProtectionBuilder ConfigureDataProtection(IDataProtectionBuilder dataProtectionBuilder)
+        {
+            base.ConfigureDataProtection(dataProtectionBuilder);
+            var redisConfig = new RedisOptions();
+            Configuration.GetSection("Redis").Bind(redisConfig);
+            if (!string.IsNullOrEmpty(redisConfig.Host))
+            {
+                dataProtectionBuilder.PersistKeysToStackExchangeRedis(() =>
+                {
+                    var redis = ConnectionMultiplexer.Connect(redisConfig.ConnectionString);
+                    return redis.GetDatabase(redisConfig.Db);
+                }, $"{Environment.ApplicationName}-DP");
+            }
+
+            return dataProtectionBuilder;
+        }
+    }
+
+    public class RedisOptions
+    {
+        public string Host { get; set; } = "";
+        public int Port { get; set; } = 6379;
+        public int Db { get; set; } = -1;
+
+        public string ConnectionString => $"{Host}:{Port}";
     }
 }
