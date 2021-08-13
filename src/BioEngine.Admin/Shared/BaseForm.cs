1188f73e8890f94a133ec9b061d2e20ce84218e3
George Drak <sonicgd@gmail.com>
2021-08-13 13:27:47 +0500

    refactor(admin): move base form to separate file

---
+++ src/BioEngine.Admin/Shared/BaseForm.cs
@@ -0,0 +1,21 @@
+﻿using BioEngine.Core.Data.Entities.Abstractions;
+using BioEngine.Core.Data.Repositories;
+using KellermanSoftware.CompareNetObjects;
+using Sitko.Blockly.Blazor.Extensions;
+using Sitko.Core.Blazor.AntDesignComponents.Components;
+using Sitko.Core.Repository;
+
+namespace BioEngine.Admin.Shared
+{
+    public abstract class BaseForm<TEntity, TEntityPk, TRepository> : BaseAntRepositoryForm<TEntity, TEntityPk,
+        TRepository>
+        where TEntity : class, IEntity<TEntityPk>, IPublishable, new()
+        where TRepository : class, IRepository<TEntity, TEntityPk>, IPublishableEntityRepository<TEntity>
+    {
+        protected override void ConfigureComparer(ComparisonConfig comparisonConfig)
+        {
+            base.ConfigureComparer(comparisonConfig);
+            comparisonConfig.AddBlocklyCollectionMapping();
+        }
+    }
+}
