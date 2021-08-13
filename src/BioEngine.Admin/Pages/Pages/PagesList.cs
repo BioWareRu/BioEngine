a5a6cd6159a879805ae080da2259ae2daa76efa3
George Drak <sonicgd@gmail.com>
2021-08-13 15:16:58 +0500

    feat(pages): show page sites in list

--- src/BioEngine.Admin/Pages/Pages/PagesList.cs
+++ src/BioEngine.Admin/Pages/Pages/PagesList.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.Threading.Tasks;
 using BioEngine.Admin.Shared;
 using BioEngine.Core.Data.Entities;
 using BioEngine.Core.Data.Repositories;
+using Sitko.Core.Repository;
 
 namespace BioEngine.Admin.Pages.Pages
 {
     public class PagesList : BaseBioEngineList<Page, Guid, PagesRepository>
     {
+        protected override async Task ConfigureQueryAsync(IRepositoryQuery<Page> query)
+        {
+            await base.ConfigureQueryAsync(query);
+            query.Include(p => p.Sites);
+        }
     }
 }
